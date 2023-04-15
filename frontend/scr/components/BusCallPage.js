93c003ed9cbe16743c92fbaaf6184f3226472267
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2023-04-15 11:12:08 +0700

    Update BusCallPage.js

--- frontend/scr/components/BusCallPage.js
+++ frontend/scr/components/BusCallPage.js
@@ -6,7 +6,7 @@ const BusCallPage = () => {
 
   useEffect(() => {
     // Fetch bus data from API
-    fetch('http://localhost:3000/buses')
+    fetch('http://34.236.143.191/:3000/buses')
       .then(response => response.json())
       .then(data => setBuses(data));
   }, []);
